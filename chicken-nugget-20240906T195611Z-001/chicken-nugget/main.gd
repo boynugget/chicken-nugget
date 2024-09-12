08a18fbce17c95629be5027176e7dd2caf4de1a8
boynugget <alexjhering2009@gmail.com>
2024-09-12 15:03:56 -0500

    fxbndouydsagxuikyrtfberuseyv4i
    gh fxjftrdg

--- chicken-nugget-20240906T195611Z-001/chicken-nugget/main.gd
+++ chicken-nugget-20240906T195611Z-001/chicken-nugget/main.gd
@@ -3,6 +3,23 @@ extends Control
 var number =0
 
 @onready var label =$MarginContainer/VBoxContainer/Label
+
+@onready var imagelist = [
+load("res://art/sprite_0.png"),
+load("res://art/sprite_1.png"),
+load("res://art/sprite_2.png"),
+load("res://art/sprite_3.png"),
+load("res://art/sprite_4.png"),
+]
+
+@onready var cool_animation: TextureRect = $MarginContainer/VBoxContainer/coolanimation
+var count =0
 func _on_button_pressed() -> void:
 	number +=1
 	label.text = str(number)
+	
+	cool_animation.texture = imagelist[count]
+	if count == 4:
+		count = 0
+	else:
+		count+=1
