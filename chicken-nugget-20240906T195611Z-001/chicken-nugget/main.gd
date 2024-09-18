57632b244f677403cc4db82b8d1575ceb631eb5f
boynugget <alexjhering2009@gmail.com>
2024-09-18 15:00:17 -0500

    /xflgboim

--- chicken-nugget-20240906T195611Z-001/chicken-nugget/main.gd
+++ chicken-nugget-20240906T195611Z-001/chicken-nugget/main.gd
@@ -23,7 +23,7 @@ func _on_button_pressed() -> void:
 	if count == 4:
 		count = 0
 		nuggets +=ammountNuggets
-		###make nugget progress here with %
+		###make nugget progress here with % 
 	else:
 		count+=1
 
@@ -38,7 +38,7 @@ func _on_upgrade_1_pressed() -> void:
 
 
 func _on_upgrade_2_pressed() -> void:
-	if ammountNuggets >= 50:
+	if nuggets >= 50:
 		ammountNuggets +=5
 		nuggets-=50
 		label.text = str(nuggets)
