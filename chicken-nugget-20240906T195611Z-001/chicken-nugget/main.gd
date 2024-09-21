40aa3b4b9442b7f29d93f0a3680ec18380155817
boynugget <alexjhering2009@gmail.com>
2024-09-21 12:07:35 -0500

    khhvjgv
    bgvgihb

--- chicken-nugget-20240906T195611Z-001/chicken-nugget/main.gd
+++ chicken-nugget-20240906T195611Z-001/chicken-nugget/main.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var tickDuration
+
 var autoNugget =0
 var nuggets =0
 var ammountNuggets = 1
@@ -20,7 +20,7 @@ load("res://art/sprite_4.png"),
 var count =1
 func _on_button_pressed() -> void:
 	
-	label.text = str(nuggets)
+	label.text = "nuggets: " + str(nuggets)
 	
 	cool_animation.texture = imagelist[count]
 	if count == 4:
@@ -36,7 +36,7 @@ func _on_upgrade_1_pressed() -> void:
 	if nuggets >= 5:
 		ammountNuggets +=1
 		nuggets-=5
-		label.text = str(nuggets)
+		label.text = "nuggets: " + str(nuggets)
 		
 		
 
@@ -46,27 +46,31 @@ func _on_upgrade_2_pressed() -> void:
 	if nuggets >= 50:
 		ammountNuggets +=5
 		nuggets-=50
-		label.text = str(nuggets)
+		label.text = "nuggets: " + str(nuggets)
 		
 
 
 		
 	
-	
+
+
 
 
 func _on_upgrade_3_pressed() -> void:
 	if nuggets >= 100:
 		ammountNuggets +=10
 		nuggets-=100
-		label.text = str(nuggets)
+		label.text = "nuggets: " + str(nuggets)
 
+func _on_timer_timeout() -> void:
+	nuggets += autoNugget
+	label.text = "nuggets: " + str(nuggets)
 
 func _on_upgrade_4_pressed() -> void:
-	if nuggets >= 1000:
-		ammountNuggets +=100
-		nuggets =-1000
-		label.text = str(nuggets)
+	if nuggets >= 150:
+		autoNugget += 1
+		nuggets  -= 150
+		label.text = "nuggets: " + str(nuggets)
 
 
 func _on_upgrade_5_pressed() -> void:
