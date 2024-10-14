d75f6ba9a5bc3685b9ee6eeb802267d7fcd026a7
boynugget <alexjhering2009@gmail.com>
2024-10-14 14:57:11 -0500

    fffffff

--- chicken-nugget-20240906T195611Z-001/chicken-nugget/main.gd
+++ chicken-nugget-20240906T195611Z-001/chicken-nugget/main.gd
@@ -2,7 +2,7 @@ extends Control
 
 var save_path = "user://variable.save"
 var autoNugget =0
-var nuggets =0
+
 var ammountNuggets = 1
 
 @onready var label =$MarginContainer/HBoxContainer/VBoxContainer/Label
@@ -20,12 +20,12 @@ load("res://art/sprite_4.png"),
 var count =1
 func _on_button_pressed() -> void:
 	
-	label.text = "nuggets: " + str(nuggets)
+	label.text = "nuggets: " + str(Global.nuggets)
 	
 	cool_animation.texture = imagelist[count]
 	if count == 4:
 		count = 0
-		nuggets +=ammountNuggets
+		Global.nuggets +=ammountNuggets
 		
 		progress_bar.value+=0.5
 	else:
@@ -33,27 +33,27 @@ func _on_button_pressed() -> void:
 		
 		if progress_bar.value == 100:
 			progress_bar.value -=100
-			autoNugget += 20
-			ammountNuggets +=100
+			autoNugget += (autoNugget*.2)
+			ammountNuggets += (ammountNuggets*.5)
 		
 		
 		
 
 func _on_upgrade_1_pressed() -> void:
-	if nuggets >= 5:
+	if Global.nuggets >= 5:
 		ammountNuggets +=1
-		nuggets-=5
-		label.text = "nuggets: " + str(nuggets)
+		Global.nuggets-=5
+		label.text = "nuggets: " + str(Global.nuggets)
 		
 		
 
 
 
 func _on_upgrade_2_pressed() -> void:
-	if nuggets >= 50:
+	if Global.nuggets >= 50:
 		ammountNuggets +=5
-		nuggets-=50
-		label.text = "nuggets: " + str(nuggets)
+		Global.nuggets-=50
+		label.text = "nuggets: " + str(Global.nuggets)
 		
 
 
@@ -64,63 +64,63 @@ func _on_upgrade_2_pressed() -> void:
 
 
 func _on_upgrade_3_pressed() -> void:
-	if nuggets >= 100:
+	if Global.nuggets >= 100:
 		ammountNuggets +=10
-		nuggets-=100
-		label.text = "nuggets: " + str(nuggets)
+		Global.nuggets-=100
+		label.text = "nuggets: " + str(Global.nuggets)
 
 func _on_timer_timeout() -> void:
-	nuggets += autoNugget
-	label.text = "nuggets: " + str(nuggets)
+	Global.nuggets += autoNugget
+	label.text = "nuggets: " + str(Global.nuggets)
 
 func _on_upgrade_4_pressed() -> void:
-	if nuggets >= 150:
+	if Global.nuggets >= 150:
 		autoNugget += 1
-		nuggets  -= 150
-		label.text = "nuggets: " + str(nuggets)
+		Global.nuggets  -= 150
+		label.text = "nuggets: " + str(Global.nuggets)
 
 
 func _on_upgrade_5_pressed() -> void:
-	if nuggets >= 300:
+	if Global.nuggets >= 300:
 		autoNugget += 5
-		nuggets -= 300
-		label.text = "nuggets: " + str(nuggets)
+		Global.nuggets -= 300
+		label.text = "nuggets: " + str(Global.nuggets)
 	
 
 	
 
 
 func _on_upgrade_6_pressed() -> void:
-	if nuggets >= 600:
+	if Global.nuggets >= 600:
 		autoNugget += 15
-		nuggets -= 600
-		label.text = "nuggets: " + str(nuggets)
+		Global.nuggets -= 600
+		label.text = "nuggets: " + str(Global.nuggets)
 		 
 
 
 func _on_upgrade_7_pressed() -> void:
-	if nuggets >=1250:
+	if Global.nuggets >=1250:
 		autoNugget +=20
 		ammountNuggets +=50
-		nuggets -=1250
-		label.text = "nuggets: " + str(nuggets)
+		Global.nuggets -=1250
+		label.text = "nuggets: " + str(Global.nuggets)
 		
 	
 func save():
 	var file = FileAccess.open(save_path, FileAccess.WRITE)
-	file.store_var(nuggets)
+	file.store_var(Global.nuggets)
 	file.store_var(ammountNuggets)
 	file.store_var(autoNugget)
 	
 func load_data():
 	if FileAccess.file_exists(save_path):
 		var file = FileAccess.open(save_path, FileAccess.READ)
-		nuggets = file.get_var(nuggets)
+		Global.nuggets = file.get_var(Global.nuggets)
 		ammountNuggets = file.get_var(ammountNuggets)
 		autoNugget = file.get_var(autoNugget)
 	else:
 		print("no data saved...")
-		nuggets = 0
+		Global.nuggets = 0
 		ammountNuggets = 0
 		autoNugget = 0
 
@@ -137,5 +137,7 @@ func _on_load_pressed() -> void:
 	load_data()
 
 
-func _on_upgrade_8_pressed() -> void:
-	get_tree().change_scene("res://pt_2.tscn")
+
+
+func _on_load_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://pt_2.tscn")
